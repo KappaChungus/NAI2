4d169795cbd4d8dffdb3b0e0db820dacd23ef76e
KappaChungus <janek.code.master@gmail.com>
2025-03-18 23:13:36 +0100

    xd

--- Perceptron/obj/Debug/net8.0-windows/Perceptron.AssemblyInfo.cs
+++ Perceptron/obj/Debug/net8.0-windows/Perceptron.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Perceptron")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44c5d38bc26ed776d989f210fd606649bffbe6d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9303339537145f56c31c5dc1b450eb4083c8e566")]
 [assembly: System.Reflection.AssemblyProductAttribute("Perceptron")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Perceptron")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
